8f571a446a143c13597ce120c35c2f2fe94e91ae
ngotheodore <ngoth@oregonstate.edu>
2024-05-05 10:30:45 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+options = [1,2,3,4,5,6]
+
+def main:
+while True:
+    print("Program Options:")
+    print("1. Add - Add an entry to the database")
+    print("2. Browse - Browse entries in the database")
+    print("3. Edit - Edit an entry in the database")
+    print("4. Delete - Delete an entry in the database")
+    print("5. Help - Provides instructions on how to use the different functions of the program")
+    print("6. Quit - Exit the program")
+
+    choice = input("\nPlease choose an option>")
+
+    if choice in options:
+        return choice
+    else:
+        print("\nError: That option is not recognized")
+        print("Please try again\n")
