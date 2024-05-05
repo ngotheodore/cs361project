b2b72c5f89a790f46e7c98afefdcabf0cf28af60
ngotheodore <ngoth@oregonstate.edu>
2024-05-05 10:48:59 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,20 @@
 options = [1,2,3,4,5,6]
 
-def main:
-while True:
-    print("Program Options:")
-    print("1. Add - Add an entry to the database")
-    print("2. Browse - Browse entries in the database")
-    print("3. Edit - Edit an entry in the database")
-    print("4. Delete - Delete an entry in the database")
-    print("5. Help - Provides instructions on how to use the different functions of the program")
-    print("6. Quit - Exit the program")
+def main_menu():
+    while True:
+        print("Program Options:")
+        print("1. Add - Add an entry to the database")
+        print("2. Browse - Browse entries in the database")
+        print("3. Edit - Edit an entry in the database")
+        print("4. Delete - Delete an entry in the database")
+        print("5. Help - Provides instructions on how to use the different functions of the program")
+        print("6. Quit - Exit the program")
 
-    choice = input("\nPlease choose an option>")
+        choice = int(input("\nPlease choose an option>"))
 
-    if choice in options:
-        return choice
-    else:
-        print("\nError: That option is not recognized")
-        print("Please try again\n")
+        if choice in options:
+            return choice
+        else:
+            print("\nError: That option is not recognized")
+            print("Please try again\n")
+        
